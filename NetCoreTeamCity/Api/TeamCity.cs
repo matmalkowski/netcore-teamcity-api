beea240e0a85cb5f1c3b571f9292cda0b3c8f125
Maciej Małkowski <monkey3310@gmail.com>
2017-01-21 00:17:14 +0700

    Small cleanup

--- NetCoreTeamCity/Api/TeamCity.cs
+++ NetCoreTeamCity/Api/TeamCity.cs
@@ -5,14 +5,13 @@ namespace NetCoreTeamCity.Api
 {
     public class TeamCity : ITeamCity
     {
-        private readonly IBuildService _buildService;
         public TeamCity(string host, string userName, string password, bool usingSSL = true)
         {
             var connectionConfig = new TeamCityConnectionSettingsBuilder().ToHost(host).UsingSSL().AsUser(userName, password).Build();
             var bootstrapper = new BootStrapper(connectionConfig);
-            _buildService = bootstrapper.Get<IBuildService>();
+            Builds = bootstrapper.Get<IBuildService>();
         }
 
-        public IBuildService Builds { get { return _buildService; } }
+        public IBuildService Builds { get; }
     }
 }
