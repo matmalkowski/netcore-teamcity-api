1d0630e50498a1b7acc72f352859ea572179969d
Kemakorn S <kemakorn.sudchukiat@agoda.com>
2017-02-26 23:46:13 +0700

    update test data

--- NetCoreTeamCity.Tests/Services/TestOccurencesServiceTests.cs
+++ NetCoreTeamCity.Tests/Services/TestOccurencesServiceTests.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using FakeItEasy;
 using FluentAssertions;
 using NetCoreTeamCity.Clients;
-using NetCoreTeamCity.Exceptions;
 using NetCoreTeamCity.Models;
 using NetCoreTeamCity.Services;
 using NUnit.Framework;
-using System.IO;
 using NetCoreTeamCity.Api;
 
 namespace NetCoreTeamCity.Tests.Services
@@ -23,51 +20,51 @@ namespace NetCoreTeamCity.Tests.Services
         {
             // Arrange
             var teamCityApiClient = A.Fake<ITeamCityApiClient>();
-            A.CallTo(() => teamCityApiClient.Get<TestOccurrence>($"{endpoint}/id:33960,build:(id:591667)")).Returns(new TestOccurrence
+            A.CallTo(() => teamCityApiClient.Get<TestOccurrence>($"{endpoint}/id:123,build:(id:123456)")).Returns(new TestOccurrence
             {
-                Id = "id:33960,build:(id:591667)",
-                Build = new Build() { Id = 591667 },
-                Test = new Test() { Id = 4015036523514658854, Name = "Agoda.Website.UnitTest.dll: Agoda.LCD.UnitTest.ManagerTest.FullDetectionManagerTest" }
+                Id = "id:123,build:(id:123456)",
+                Build = new Build() { Id = 123456 },
+                Test = new Test() { Id = 4015036523514658854, Name = "UnitTest" }
 
             });
 
             var testOccurenceService = new TestOccurrencesService(teamCityApiClient);
 
             // Act
-            var testOccObj = testOccurenceService.Get("id:33960,build:(id:591667)");
+            var testOccObj = testOccurenceService.Get("id:123,build:(id:123456)");
 
             // Assert
             testOccObj.Should().NotBeNull();
-            testOccObj.Id.Should().Be("id:33960,build:(id:591667)");
-            testOccObj.Build.Id.Should().Be(591667);
+            testOccObj.Id.Should().Be("id:123,build:(id:123456)");
+            testOccObj.Build.Id.Should().Be(123456);
             testOccObj.Test.Id.Should().Be(4015036523514658854);
-            testOccObj.Test.Name.Should().Be("Agoda.Website.UnitTest.dll: Agoda.LCD.UnitTest.ManagerTest.FullDetectionManagerTest");
+            testOccObj.Test.Name.Should().Be("UnitTest");
         }
 
         [Test]
         public void Find_ByBuildId_NoField()
         {
             var teamCityApiClient = A.Fake<ITeamCityApiClient>();
-            A.CallTo(() => teamCityApiClient.Get<TestOccurrences>($"{endpoint}?locator=build:(id:591667)"))
+            A.CallTo(() => teamCityApiClient.Get<TestOccurrences>($"{endpoint}?locator=build:(id:123456)"))
                 .Returns(new TestOccurrences
                 {
                     TestOccurrenceItems = new List<TestOccurrence>
                     {
-                        new TestOccurrence {Id="id:123,build:(id:591667)" },
-                        new TestOccurrence {Id="id:124,build:(id:591667)" }
+                        new TestOccurrence {Id="id:123,build:(id:123456)" },
+                        new TestOccurrence {Id="id:124,build:(id:123456)" }
                     }
                 });
 
             var testOccurenceService = new TestOccurrencesService(teamCityApiClient);
 
             // Act
-            var testOccList = testOccurenceService.Find(By.Build.Id(591667));
+            var testOccList = testOccurenceService.Find(By.Build.Id(123456));
 
             // Assert
             testOccList.Should().NotBeNull();
             testOccList.Count.Should().Be(2);
-            testOccList[0].Id.Should().Be("id:123,build:(id:591667)");
-            testOccList[1].Id.Should().Be("id:124,build:(id:591667)");
+            testOccList[0].Id.Should().Be("id:123,build:(id:123456)");
+            testOccList[1].Id.Should().Be("id:124,build:(id:123456)");
         }
        
     }
